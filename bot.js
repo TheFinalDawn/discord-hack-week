8f5e14db60a902f438a387cfd0fe4c488fc3abfd
IAmDefinitelyNotARobot <ajwaite06@gmail.com>
2019-06-25 10:56:48 -0700

    Switch to Discord.js

--- bot.js
+++ bot.js
@@ -14,47 +14,24 @@ logger.add(new logger.transports.Console, {
 });
 
 logger.level = 'debug';
+const Discord = require('discord.js');
+const client = new Discord.Client(
+  token: auth.token,
+  autorun: true
+);
+ client.on('ready', function (evt) {
+     logger.info('Connected');
+     logger.info('Logged in as: ');
+     logger.info(bot.username + ' - (' + bot.id + ')');
+ });
+client.on('message', message => {
+  switch (message.content) {
+    case 'ping':
+      message.channel.send('pong')
+      break;
+    case '$help':
+      message.channel.send(`ping, $help.`)
+      break;
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // The $ is the prefix. Could make it changable.
-    let prefix = "d$";
-    if (message.substring(0, 2) == prefix && !message.author.bot) {
-        var args = message.substring(1).split('/ +/');
-        var cmd = args[0];
-
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'help':
-                bot.sendMessage({
-                    to: channelID,
-                    message: '**General Commands**\nNothing here yet...\n**Debug Commands (hint, they start with $debug!cmdname)**\n$debug!args-test'
-                    // Update every time you add a cmd.
-                });
-            break;
-            case 'debug!args-test':
-              if (!args.length) {
-                bot.sendMessage({
-                  to: channelID,
-                  message: `You didn't provide any arguments, ${message.author.userID}!`
-                });
-              }
-              message.channel.send(`Command name: ${cmd}\nArguments: ${args}`);
-              break;
-            // Add more cases to add more commands.
-
-         }
-     }
+  }
 });
