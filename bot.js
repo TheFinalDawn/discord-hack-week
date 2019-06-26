e796c17ffc79a0c047b07f85727e89acf6cce81c
IAmDefinitelyNotARobot <ajwaite06@gmail.com>
2019-06-26 08:50:01 -0700

    0.2.1!

--- bot.js
+++ bot.js
@@ -11,7 +11,14 @@ const softwaregore = [
   "https://i.redd.it/dq4q290y0c331.png",
   "https://i.imgur.com/ZAyentM.jpg",
   "https://i.redd.it/3686d9yldk031.jpg",
-]
+]; // Understand that I have no creativity.
+const meme = [
+  "https://i.redd.it/lv8a11cysp631.png",
+  "https://i.redd.it/lyavd2kxsp631.jpg",
+  "https://i.redd.it/8mr65wwssp631.jpg",
+  "https://i.redd.it/kq7j8d7ssp631.jpg",
+  "https://i.redd.it/ycl58pvnsp631.jpg",
+];
 // load materials
 var logger = require('winston');
 var config = require('./config.json');
@@ -42,7 +49,7 @@ bot.on('message', message => {
       logger.info('called ping');
       break;
     case 'help':
-      message.channel.send('ping: returns pong.\nd!help, this command.\nd!annoy, spam the target with messages.\nd!softwaregore, get an image from softwaregore (suggested by people like you!). Also check d!software-suggest.\nd!software-suggest, suggest an image to add to d!softwaregore!');
+      message.channel.send('ping: returns pong.\nd!help, this command.\nd!annoy, spam the target with messages.\nd!softwaregore, get an image from softwaregore (suggested by people like you!). Also check d!software-suggest.\nd!software-suggest, suggest an image to add to d!softwaregore!\nd!cmd-suggest, suggest a command to be added.\nd!meme, get a meme. Also see d!meme-suggest.\nd!meme-suggest, you know the drill, added these commands because I\'m lazy and uncreative.');
       logger.info('called d!help');
       break;
     case 'annoy':
@@ -66,6 +73,30 @@ bot.on('message', message => {
       break;
     case 'software-suggest':
       message.channel.send(`${message.author.toString()} Go check out https://forms.gle/pfEn2cM7SYHrcRjD8 to suggest an addition. Thank you in advance!`)
+      logger.info('Called d!software-suggest. Go check the Google forms quick!');
+      break;
+    case 'meme':
+      message.channel.send('Okey', {
+        file: meme[Math.floor(Math.random() * meme.length)]
+      });
+      logger.info('Called d!meme.')
+      break;
+    case 'cmd-suggest':
+      if (message.author.id == config.owner) {
+        message.channel.send('Dude. You can literally make the command, you have token!');
+      } else if (args.length > 0) {
+        message.channel.send("Suggestion sent via terminal to the dev.");
+        logger.info(`New Command Suggested. Info: ${args}`);
+      } else if (args.length < 1) {
+        message.channel.send(`${message.author.toString()} I kinda need a command to send.`);
+      } else {
+        message.channel.send(`Something broke. Number of Arguments: ${args.length}. Args: ${args}.`);
+        logger.warn(`Command cmd-suggest encountered an error. Args: ${args}. Count: ${args.length}.`);
+      }
+      break;
+    case 'meme-suggest':
+      message.channel.send(`${message.author.toString()} Please fill out this form: https://forms.gle/wQYNJ7sR6hmAYSbf8`);
+      logger.info('New meme suggested for d!meme. Check Google Forms.');
       break;
   }
 });
