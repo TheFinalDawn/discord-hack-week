e9097f54c3b135c1176887f017e007a39658e157
IAmDefinitelyNotARobot <ajwaite06@gmail.com>
2019-06-26 17:41:00 -0700

    0.3.0! (description) added d!dice and d!bug. Add updated logo, and added appropriate error-prevention in d!dice.

--- bot.js
+++ bot.js
@@ -33,23 +33,29 @@ logger.level = 'debug';
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 bot.login(config.token);
- bot.on('ready', function (evt) {
+ bot.on('ready', function (evt) { // connecting success.
      logger.info('Connected');
      logger.info('Logged in as: ');
      logger.info(bot.username + ' - (' + bot.id + ')');
+     bot.user.setActivity(`Derping around on ${bot.guilds.size} servers`)
  });
+ bot.on("guildCreate", guild => { // joined a server
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+});
+
+bot.on("guildDelete", guild => { // left/removed from a server
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+});
  const prefix = "d!";
-bot.on('message', message => {
-  if (message.author == bot.user) {return} // Just to avoid issues
+bot.on('message', async message => {
+  if (message.author.bot || message.content.indexOf(prefix) !== 0) {return}; // Just to avoid issues
   const args = message.content.slice(prefix.length).split(' ');
   const cmd = args.shift().toLowerCase();
   switch (cmd) {
-    case 'ping':
-      message.channel.send('pong');
-      logger.info('called ping');
-      break;
     case 'help':
-      message.channel.send('ping: returns pong.\nd!help, this command.\nd!annoy, spam the target with messages.\nd!softwaregore, get an image from softwaregore (suggested by people like you!). Also check d!software-suggest.\nd!software-suggest, suggest an image to add to d!softwaregore!\nd!cmd-suggest, suggest a command to be added.\nd!meme, get a meme. Also see d!meme-suggest.\nd!meme-suggest, you know the drill, added these commands because I\'m lazy and uncreative.');
+      message.channel.send('d!help, this command.\nd!annoy, spam the target with messages.\nd!softwaregore, get an image from softwaregore (suggested by people like you!). Also check d!software-suggest.\nd!software-suggest, suggest an image to add to d!softwaregore!\nd!cmd-suggest, suggest a command to be added.\nd!meme, get a meme. Also see d!meme-suggest.\nd!meme-suggest, you know the drill, added these commands because I\'m lazy and uncreative.\nd!dice, roll a number. (run d!dice -h for info.)\nd!bug, alert the dev to an error (or generally something strange) that happened.');
       logger.info('called d!help');
       break;
     case 'annoy':
@@ -98,8 +104,36 @@ bot.on('message', message => {
       message.channel.send(`${message.author.toString()} Please fill out this form: https://forms.gle/wQYNJ7sR6hmAYSbf8`);
       logger.info('New meme suggested for d!meme. Check Google Forms.');
       break;
-    default:
-    message.channel.send('Sorry, I didn\'t understand that. We\'re you talking to me?');
-    logger.warn('Failed to call command.');
+      case 'dice':
+        message.channel.send('Rolling...');
+        if (args[0] > 2000000000 || args[1] > 2000000000) {
+          return message.channel.send('Sorry, can\'t roll numbers bigger than 2 billion.\n*hah, you thought you could break me...*');
+        }
+        if (args.length == 2) {
+          message.channel.send(`Rolled a ${Math.floor(Math.random() * (args[1] - args[0] + 1) + args[0])}.`);
+          logger.info(`rolled between ${args[0]} and ${args[1]}`);
+        } else if (args.length == 1 && args[0] != "-h") {
+          message.channel.send(`Rolled a ${Math.floor(Math.random() * args[0] + 1)}`);
+          logger.info(`Rolled between 1 and ${args[0]}`);
+        } else if (args[0] == "-h") {
+          message.channel.send('Run only d!dice to get a number between 1 and 10. Provide 1 number to get a number between 0 and that number. Give me a minimum and a maximum to get a number between the two.');
+        } else {
+          message.channel.send(`Rolled a ${Math.floor(Math.random() * 10 + 1)}`);
+          logger.info('Rolled between 1 and 10');
+        }
+        break;
+      case 'bug':
+        if (!args.length) {
+          message.channel.send(`Could you explain what happened?`);
+        } else {
+          message.channel.send(`Note sent to the dev.`);
+          let notice = args.join(" ");
+          console.warn(`---ALERT---\nBug reported by ${message.author.toString()}.`);
+          console.warn(`Details: ${notice}\n---END ALERT---`);
+        }
+        break;
+      case '':
+        message.channel.send('Uhh...')
+        break;
   }
 });
